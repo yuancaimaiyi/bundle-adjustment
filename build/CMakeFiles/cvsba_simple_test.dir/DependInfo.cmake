088198dbb8e2c1955142cc30a482e07caba9ad02
yuancaimaiyi <1048331019@qq.com>
2019-02-28 10:22:22 +0800

    addfile

---
+++ build/CMakeFiles/cvsba_simple_test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yuancaimaiyi/图片/cvsba_simple_test/cvsba_simple_test.cpp" "/home/yuancaimaiyi/图片/cvsba_simple_test/build/CMakeFiles/cvsba_simple_test.dir/cvsba_simple_test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/yuancaimaiyi/anaconda3/include"
+  "/home/yuancaimaiyi/anaconda3/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cvsba_simple_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvsba_simple_test.dir/cvsba_simple_test.cpp.o"
+  "cvsba_simple_test.pdb"
+  "cvsba_simple_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvsba_simple_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
